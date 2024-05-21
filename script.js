eba17aa22b0f7fa5989dfe0675c436079871d7c4
Marijo Kis <marijo.kis@q.agency>
2024-05-21 20:12:27 +0200

    chore: init

---
+++ script.js
@@ -0,0 +1,15 @@
+const boja = "plava";
+
+switch (boja) {
+  case "zuta":
+    console.log("Odabrao si zutu boju");
+    break;
+  case "crvena":
+    console.log("Odabrao si crvenu boju");
+    break;
+  case "zelena":
+    console.log("Odabrao si zelenu boju");
+    break;
+  default:
+    console.log("ne znam koju si boju odabrao");
+}
